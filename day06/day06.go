a7bf8f79e240fdfbf1638aea4a8e121baa3730de
Tyler Daines <tdaines@uplynk.com>
2025-12-11 16:18:06 -0800

    Day 06, Part 02

--- day06/day06.go
+++ day06/day06.go
@@ -24,6 +24,12 @@ func Part1() (answer int, elapsed time.Duration) {
 func Part2() (answer int, elapsed time.Duration) {
 	var start = time.Now()
 
+	var lines = strings.Split(input, "\n")
+	lines = lines[:len(lines)-1]
+
+	var numbers, operations = ParseWorksheetRtL(lines)
+	answer = SolveWorksheetRtL(numbers, operations)
+
 	return answer, time.Since(start)
 }
 
@@ -82,6 +88,98 @@ func mult(numbers [][]int, col int) (product int) {
 	product = 1
 	for row := 0; row < len(numbers); row++ {
 		product *= numbers[row][col]
+	}
+
+	return
+}
+
+func ParseWorksheetRtL(lines []string) (numbers [][]int, operations []byte) {
+	var indices = getColumnIndices(lines[len(lines)-1])
+
+	for i := 0; i < len(indices); i++ {
+		var colStart = indices[i]
+		var colEnd = len(lines[0])
+		if i < len(indices)-1 {
+			colEnd = indices[i+1] - 2
+		}
+
+		numbers = append(numbers, parseNumbersRtL(lines, colStart, colEnd))
+	}
+
+	operations = parseOperations(lines[len(lines)-1])
+
+	return
+}
+
+func parseNumbersRtL(lines []string, colStart, colEnd int) (numbers []int) {
+	//
+	// 123 328  51 64
+	//  45 64  387 23
+	//   6 98  215 314
+	// *   +   *   +
+
+	//
+	// 724 2384 57 32 142 25  89  2 45  221 382 9887   24 64 723 66   31
+	// 257 7211 49 81 994 872 64  7 841 943 348 8834 1829 29 283 6   143
+	// 268 158  96 92 335 931 86 29 957 346   4  614 1872 51 576 1   876
+	// 685 51   8  53 2   383 7  55 451 939   1   79 3192 21 654 3  3822
+	// *   +    *  +  *   +   *  *  *   *   +   +    +    *  *   *  +
+	//
+
+	for i := colStart; i <= colEnd; i++ {
+		numbers = append(numbers, parseNumberRtL(lines, i))
+	}
+
+	return
+}
+
+func parseNumberRtL(lines []string, col int) (num int) {
+	for row := 0; row < len(lines)-1; row++ {
+		if col < len(lines[row]) {
+			var digit = lines[row][col]
+			if '0' <= digit && digit <= '9' {
+				num = (num * 10) + int(digit-'0')
+			}
+		}
+	}
+
+	return
+}
+
+func getColumnIndices(operations string) (indices []int) {
+	for i := 0; i < len(operations); i++ {
+		if operations[i] == '*' || operations[i] == '+' {
+			indices = append(indices, i)
+		}
+	}
+
+	return
+}
+
+func SolveWorksheetRtL(numbers [][]int, operations []byte) (total int) {
+	for i, op := range operations {
+		if op == '+' {
+			total += sumRtL(numbers[i])
+		} else {
+			total += multRtL(numbers[i])
+		}
+	}
+
+	return
+}
+
+func sumRtL(numbers []int) (sum int) {
+	for _, val := range numbers {
+		sum += val
+	}
+
+	return
+}
+
+func multRtL(numbers []int) (product int) {
+	product = 1
+	for _, val := range numbers {
+		product *= val
 	}
 
 	return
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestParseWorksheet(t *testing.T) {
 	var worksheet = []string{
-		"123 328  51 64",
-		" 45 64  387 23",
+		"123 328  51 64 ",
+		" 45 64  387 23 ",
 		"  6 98  215 314",
 		"*   +   *   +  ",
 	}
@@ -27,8 +27,8 @@ func TestParseWorksheet(t *testing.T) {
 
 func TestSolverWorksheet(t *testing.T) {
 	var worksheet = []string{
-		"123 328  51 64",
-		" 45 64  387 23",
+		"123 328  51 64 ",
+		" 45 64  387 23 ",
 		"  6 98  215 314",
 		"*   +   *   +  ",
 	}
@@ -37,4 +37,37 @@ func TestSolverWorksheet(t *testing.T) {
 	var total = day06.SolveWorksheet(numbers, operations)
 
 	assert.Equal(t, 4277556, total)
+}
+
+func TestParseWorksheetRtL(t *testing.T) {
+	var worksheet = []string{
+		"123 328  51 64 ",
+		" 45 64  387 23 ",
+		"  6 98  215 314",
+		"*   +   *   +  ",
+	}
+
+	var numbers, operations = day06.ParseWorksheetRtL(worksheet)
+
+	assert.Equal(t, 4, len(numbers))
+	assert.Equal(t, []int{1, 24, 356}, numbers[0])
+	assert.Equal(t, []int{369, 248, 8}, numbers[1])
+	assert.Equal(t, []int{32, 581, 175}, numbers[2])
+	assert.Equal(t, []int{623, 431, 4}, numbers[3])
+
+	assert.Equal(t, []byte{'*', '+', '*', '+'}, operations)
+}
+
+func TestSolverWorksheetRtL(t *testing.T) {
+	var worksheet = []string{
+		"123 328  51 64 ",
+		" 45 64  387 23 ",
+		"  6 98  215 314",
+		"*   +   *   +  ",
+	}
+
+	var numbers, operations = day06.ParseWorksheetRtL(worksheet)
+	var total = day06.SolveWorksheetRtL(numbers, operations)
+
+	assert.Equal(t, 3263827, total)
 }
